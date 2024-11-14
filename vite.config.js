5c3795f72aa6f9aeac0736542da961ff6243b940
Augusto <augusto.rocha004@gmail.com>
2024-11-14 19:00:47 -0300

    Energia

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/GS-energiasrenovaveis/',  // Substitua pelo nome do seu repositório
+})
+
